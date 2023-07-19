b6f4647cf3bbbfacdb6b550e0ba74cbd9a07e799
zhaomn33 <zhaomn_314@163.com>
2023-07-19 14:50:49 +0800

    feat: init

---
+++ src/Home/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
+import 'antd/dist/antd.css'
+import './index.css'
+import logo from '../logo.svg'
+
+const demoReact = () => {
+  return (
+    <div>
+      demo
+    </div>
+  )
+}
+
+export default demoReact
